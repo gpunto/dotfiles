f5a1679da27677a5f519beeadf1a27de893a9458
G <gianmarco.david@gmail.com>
2022-12-13 18:48:39 +0100

    Add neoclip plugin to neovim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,11 +1,11 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-    install_path })
-  print("Installing packer close and reopen NeoVim...")
+	PACKER_BOOTSTRAP =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	print("Installing packer close and reopen NeoVim...")
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -19,82 +19,89 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init({
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end
-  }
-}
-)
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "single" })
+		end,
+	},
+})
 
 return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use { "ellisonleao/gruvbox.nvim" }
+	use("wbthomason/packer.nvim")
+	use({ "ellisonleao/gruvbox.nvim" })
 
-  use "nvim-lua/plenary.nvim"
+	use("nvim-lua/plenary.nvim")
 
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "saadparwaiz1/cmp_luasnip"
-  use "hrsh7th/cmp-nvim-lsp"
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
 
-  use "L3MON4D3/LuaSnip"
-  use "rafamadriz/friendly-snippets"
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-  use "neovim/nvim-lspconfig"
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  use "jose-elias-alvarez/null-ls.nvim"
-  use {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end
-  }
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate"
-  }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use "nvim-telescope/telescope-fzf-native.nvim"
-  use { "nvim-telescope/telescope-file-browser.nvim" }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("nvim-telescope/telescope-fzf-native.nvim")
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	use({
+		"AckslD/nvim-neoclip.lua",
+		requires = {
+			-- { "kkharji/sqlite.lua", module = "sqlite" },
+			{ "kkharji/sqlite.lua" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+	})
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup()
-    end
-  }
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup()
+		end,
+	})
 
-  use {
-    "phaazon/hop.nvim",
-    branch = 'v2',
-    config = function()
-      require("hop").setup()
-    end
-  }
+	use({
+		"phaazon/hop.nvim",
+		branch = "v2",
+		config = function()
+			require("hop").setup()
+		end,
+	})
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  use { "akinsho/toggleterm.nvim", tag = '*' }
+	use({ "akinsho/toggleterm.nvim", tag = "*" })
 
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)

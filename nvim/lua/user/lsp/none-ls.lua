357d1a9d8923482c4742c1fb60f6b28fbbac84cc
G <gianmarco.david@gmail.com>
2024-01-05 17:49:14 +0100

    Replace null-ls with none-ls

---
+++ nvim/lua/user/lsp/none-ls.lua
@@ -0,0 +1,17 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+
+    null_ls.setup({
+      debug = false,
+      sources = {
+        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.stylua,
+      },
+    })
+
+  end
+}
--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -1,42 +1,41 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+  "ellisonleao/gruvbox.nvim",
 
-	"nvim-lua/plenary.nvim",
+  "nvim-lua/plenary.nvim",
 
-	"neovim/nvim-lspconfig",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"jose-elias-alvarez/null-ls.nvim",
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
 
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
 
-	{
-		"ggandor/leap.nvim",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
-	},
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
 
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup()
-		end,
-	},
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
 
-	{
-		"kylechui/nvim-surround",
-		config = function()
-			require("nvim-surround").setup({
-				keymaps = {
-					visual = "gS",
-				},
-			})
-		end,
-	},
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          visual = "gS",
+        },
+      })
+    end,
+  },
 }

676bfaf48e35d4a1f42b7ce323dc962d94ab7336
Gianmarco David <gianmarco.david@glovoapp.com>
2024-01-08 13:02:28 +0100

    Fix lazy.nvim warnings

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "kyazdani42/nvim-web-devicons" },
-	config = {
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
 		options = {
 			icons_enabled = true,
 			theme = "auto",

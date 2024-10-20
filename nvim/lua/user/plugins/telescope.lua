0ada88417f973fa052fa1124bb417a0a80f06f59
Gian <47775302+gpunto@users.noreply.github.com>
2024-10-20 09:41:20 +0200

    Only show folder's :oldfiles in Telescope

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,35 +1,42 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("user.neoclip")
-			local telescope = require("telescope")
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.4",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("user.neoclip")
+      local telescope = require("telescope")
 
-			telescope.load_extension("file_browser")
-			telescope.load_extension("neoclip")
+      telescope.load_extension("file_browser")
+      telescope.load_extension("neoclip")
 
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>fb",
-				":Telescope buffers<CR> initial_mode=normal<CR>",
-				{ noremap = true }
-			)
-			vim.api.nvim_set_keymap("n", "<leader>fl", ":Telescope live_grep<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("n", "<leader>fF", ":Telescope file_browser<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope oldfiles<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("n", "<leader>fp", ":Telescope neoclip initial_mode=normal<CR>", { noremap = true })
-		end,
-	},
-	"nvim-telescope/telescope-fzf-native.nvim",
-	"nvim-telescope/telescope-file-browser.nvim",
-	{
-		"AckslD/nvim-neoclip.lua",
-		dependencies = {
-			{ "kkharji/sqlite.lua" },
-			{ "nvim-telescope/telescope.nvim" },
-		},
-	},
+      telescope.setup({
+        pickers = {
+          oldfiles = {
+            cwd_only = true
+          }
+        }
+      })
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>fb",
+        ":Telescope buffers<CR> initial_mode=normal<CR>",
+        { noremap = true }
+      )
+      vim.api.nvim_set_keymap("n", "<leader>fl", ":Telescope live_grep<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>fF", ":Telescope file_browser<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope oldfiles<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>fp", ":Telescope neoclip initial_mode=normal<CR>", { noremap = true })
+    end,
+  },
+  "nvim-telescope/telescope-fzf-native.nvim",
+  "nvim-telescope/telescope-file-browser.nvim",
+  {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+      { "kkharji/sqlite.lua" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+  },
 }

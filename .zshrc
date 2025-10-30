e25edb012a5c4aaa28bac0d6720e18ed4d43df66
Gian <47775302+gpunto@users.noreply.github.com>
2025-10-30 23:47:33 +0100

    Extend & improve hyprland config

--- .zshrc
+++ .zshrc
@@ -62,9 +62,9 @@ ZSH_THEME="spaceship"
 
 # Homebrew config
 # Needed before sourcing omz to make completions work
-eval "$(brew shellenv)"
+# eval "$(brew shellenv)"
 # Enforces using brew-installed commands before system ones
-export PATH="/opt/homebrew/bin:$PATH"
+# export PATH="/opt/homebrew/bin:$PATH"
 
 # Which plugins would you like to load?
 # Standard plugins can be found in ~/.oh-my-zsh/plugins/*
---
+++ rofi/powermenu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+shutdown=$'\tShutdown'
+reboot=$'\tReboot'
+suspend=$'󰒲\tSuspend'
+logout=$'󰿅\tLogout'
+
+rofi_cmd() {
+    rofi -dmenu -p "Power Menu"
+}
+
+chosen="$(echo -e "$suspend\n$logout\n$shutdown\n$reboot" | rofi_cmd)"
+
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $suspend)
+        systemctl suspend
+        ;;
+    $logout)
+        hyprctl dispatch exit
+        ;;
+esac

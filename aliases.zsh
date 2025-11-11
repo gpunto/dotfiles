c0fab3c5d2651453798cf5bd1cc762f97d6a02ec
Gian <47775302+gpunto@users.noreply.github.com>
2025-11-11 12:16:01 +0100

    Remove unneeded aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,18 +2,9 @@ alias g='git'
 alias rebdev='git pull origin develop --rebase=i'
 alias rebmas='git pull origin master --rebase=i'
 alias rebmai='git pull origin main --rebase=i'
-alias rebc='git rebase --continue'
-alias rebab='git rebase --abort'
 alias pushnew='g push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias ck='g checkout'
-alias ckb='g checkout -b'
-alias pf='g push --force-with-lease'
-
-alias h='history' 
-alias hg='history | grep'
 
 alias v='nvim'
 
 alias gw='./gradlew'
 
-alias sudo='sudo '
